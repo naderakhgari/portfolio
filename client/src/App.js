0ebc455f8009273806deec2c90088cf44c9e17df
Nader Akhgari <nader90akhgari@gmail.com>
2020-10-09 15:46:22 +0100

    this is the initial of the app

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Nader Akhgari
+        </p>
+      </header>
+      <body className="container body-container">
+<h1>This is the portfolio body</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
