d470ecf7f7014521a9818f833f79d1e2d8dfef05
Nader Akhgari <nader90akhgari@gmail.com>
2020-10-11 12:04:41 +0100

    added the first endpoing

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.get("/test",(req, res)=>{
+    res.send("hello !")
+} )
+
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}!`)
 })
