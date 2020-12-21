beaec7b9d374673ccc4f99faa191b2b2f6f1daf2
Nader Akhgari <nader90akhgari@gmail.com>
2020-12-21 00:32:50 +0000

    change the footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./App.css";
 import "./grid.css";
 
-import {FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import Projects from "./Components/Projects";
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Home from "./Components/Home";
@@ -16,35 +16,41 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="header-title">
-        <p>Nader Akhgari</p>
+          <p>Nader Akhgari</p>
         </div>
         <div className="header-links">
-          <a href="/" className="header-link"><p>Home</p></a>
-        <a href="/contact" className="header-link"><p>Contact</p></a>
-        <a href="/about" className="header-link"><p>About</p></a>
+          <a href="/" className="header-link">
+            <p>Home</p>
+          </a>
+          <a href="/contact" className="header-link">
+            <p>Contact</p>
+          </a>
+          <a href="/about" className="header-link">
+            <p>About</p>
+          </a>
         </div>
       </header>
       <Router className="main">
-				<div className="body-container">
-					<Switch>
-						<Route exact path="/about">
-							<About />
-						</Route>
+        <div className="body-container">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
             <Route exact path="/contact">
-							<Contact />
-						</Route>
-						<Route exact path="/">
-							<Home />
-						</Route>
+              <Contact />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
             <Route exact path="/projects">
-							<Projects />
-						</Route>
+              <Projects />
+            </Route>
             <Route exact path="/cv">
-							<Cv />
-						</Route>
-					</Switch>
-				</div>
-			</Router>
+              <Cv />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       {/* <div className="container body-container">
         <div>
         <p>This is the portfolio body</p>
@@ -53,9 +59,30 @@ function App() {
       </div> */}
       <footer className="App-footer">
         <div className="footer-icons">
-        <a href="https://github.com/naderakhgari" className="nolink" rel="noopener noreferrer"><FaGithub /></a>
-        <a href="https://www.linkedin.com/in/nader-akhgari/" className="nolink" rel="noopener noreferrer"><FaLinkedin /></a>
-        <a href="https://twitter.com/AkhgariNader" className="nolink" rel="noopener noreferrer"><FaTwitter /></a>
+          <a
+            href="https://github.com/naderakhgari"
+            className="nolink"
+            rel="noopener noreferrer"
+            id="github"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nader-akhgari/"
+            className="nolink"
+            rel="noopener noreferrer"
+            id="linkedin"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://twitter.com/AkhgariNader"
+            className="nolink"
+            rel="noopener noreferrer"
+            id="twitter"
+          >
+            <FaTwitter />
+          </a>
         </div>
       </footer>
     </div>
