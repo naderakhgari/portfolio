3a1f83dcb0f706fe7c449d093d3ef73a9650174f
Nader Akhgari <nader90akhgari@gmail.com>
2021-01-04 13:07:23 +0000

    change the about me text

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Nader from "../img/nader.jpg"
+import Nader from "../img/nader.jpg";
 
 const About = () => {
   return (
@@ -7,16 +7,17 @@ const About = () => {
       <div className="col-8">
         <p className="about-me">About me</p>
         <p className="about-me-text">
-          I am a proactive, dedicated, self-motivated person with a wide range
-          of transferable skills in IT infrastructure. Strong team member
-          capable of adapting well to new circumstances and conditions. I am
-          comfortable with all aspects of learning and can adapt to new
-          environments quickly. Currently seeking a role as a full-stack
-          developer.
+          Dedicated full stack developer with one year experience in building
+          scalable web applications and databases after completing
+          CodeYourFuture Bootcamp. I have a degree in BEng Software Engineering
+          and experience in full-stack technologies. Self-motivated person with
+          a wide range of transferable skills in IT infrastructure and a strong
+          team player. Seeking to further improve my programming skills and open
+          to opportunities to work as Full Stack Developer.
         </p>
       </div>
       <div className="col-4">
-          <img src={Nader} alt="nader" className="nader-img col-12"/>
+        <img src={Nader} alt="nader" className="nader-img col-12" />
       </div>
     </div>
   );
